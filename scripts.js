a4d0bd57defe791f55b8e99dd0a6439f8af7c92c
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-08-02 11:31:52 +0300

    banner zoom-in/zoom-out

--- scripts.js
+++ scripts.js
@@ -11,6 +11,27 @@ function OpenMenu(button) {
 }
 
 jQuery(document).ready(function() {
+    const Banner = jQuery(".ArticleBanner");
+    const Modal = jQuery(".ArticleBannerModal");
+    const ModalContent = jQuery(".ArticleBannerModalContent");
+
+    function getDistanceToTop(element) {
+        var elementOffset = $(element).offset().top;
+        var windowScrollTop = $(window).scrollTop();
+        var distance = elementOffset - windowScrollTop;
+        return distance;
+    }
+
+    function ImageZoomOut(){
+        Banner.attr("data-rmiz-wrap", "visible");
+        Modal.attr("data-rmiz-overlay", "false");
+        ModalContent.attr("data-rmiz-modal-content", "false");
+        ModalContent.css({
+            "transition-duration": "300ms",
+            transform: `scale(1) translate(0px, 0px)`
+        });
+    }
+
     jQuery(".DropDownBtn").hover(function(){
         OpenMenu(jQuery(this))
     });
@@ -39,7 +60,36 @@ jQuery(document).ready(function() {
                 } else {
                     navbar.css('top', 'clamp(24px,1.6666666667vw,32px)');
                 }
-            lastScroll = currentScroll <= 0 ? 0 : currentScroll;
+                lastScroll = currentScroll <= 0 ? 0 : currentScroll;
         }
     })
+
+    jQuery(Banner).on("click", function(){
+        const BannerHeight = Banner.innerHeight();
+        const BannerWidth = Banner.innerWidth();
+        const BannerOffsetLeft = Banner.offset().left;
+        const BannerOffsetTop = getDistanceToTop(Banner);
+        const Scale = (window.innerWidth * 0.7) / BannerWidth;
+        const ScaledElement = BannerHeight * Scale;
+        const ScaledElementTopPosition = (visualViewport.height - ScaledElement) / 2;
+        const ScaledElementDiff = (ScaledElement - BannerHeight) / 2;
+        const TranslateY = ((ScaledElementTopPosition + ScaledElementDiff) - BannerOffsetTop) / Scale;
+
+        Banner.attr("data-rmiz-wrap", "hidden");
+        ModalContent.attr("data-rmiz-modal-content", "true");
+        Modal.attr("data-rmiz-overlay", "true");
+
+        ModalContent.css({
+            height: BannerHeight,
+            width: BannerWidth,
+            top: BannerOffsetTop,
+            left: BannerOffsetLeft,
+            "transition-duration": "300ms",
+            transform: `scale(${Scale}) translate(0px, ${TranslateY}px)`,
+        });
+        
+        jQuery(window).on("scroll", ImageZoomOut);
+    });
+
+    jQuery(".ArticleBannerModal button").on("click", ImageZoomOut);
 })
